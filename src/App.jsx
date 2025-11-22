337ee1a423f33c4ecf5920a10e4c991e006f11dc
sney01308-sketch <sney01308@gmail.com>
2025-11-22 00:05:36 -0300

    Create App component with welcome message

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px', fontSize: '2rem' }}>
+      <h1>Olá mundo! Meu site está no ar! 🚀</h1>
+      <p>Deploy feito com Vercel + GitHub em minutos</p>
+    </div>
+  )
+}
+
+export default App
